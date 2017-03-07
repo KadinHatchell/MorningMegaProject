b247ac9a962978d94dacb4e58013db5373f25e9c
Kadin Hatchell <kadinahatchell@gmail.com>
2017-03-07 08:47:56 -0700

    finished it

--- Queue.h
+++ Queue.h
@@ -7,9 +7,62 @@
 
 #ifndef QUEUE_H_
 #define QUEUE_H_
+#include <assert>
+#include "DoubleLinkedList.h"
+template <class Type>
+class Queue : public DoublyLinkedList<Type>
+{
 
+};
+template <class Type>
+Type Queue<Type> :: remove(int index)
+{
+    assert(index == 0 && this->getSize() > 0);
+    return dequeue();
+}
+/*
+ 1. assert size is valid
+ 2. get data from front node
+ 3. if size == 1, set end to nullptr
+ 4. else, move front to fronts next
+ 4a set fronts previous to nullptr.
+ 5. delete old fornt ndoe
+ 6. adjsut size down by 1
+ 7. return old value.
+ */
+template <class Type>
+Type Queue<Type> :: dequeue()
+{
+    assert(this->getSize() > 0);
 
+    Type removedValue = this->getFront()->getNodeData();
+    BiDirectionalNode<Type> * removeMe = this->getFront();
 
+    if(this->getSize() ==1)
+    {
+        this->setEnd(nullptr);
+        this->setFront(nullptr);
+    }
+    else
+    {
+        this->setFront(removeMe->getNextPointer());
+    }
+    this->setFront()->setPreviousPointer(nullptr);
 
+    delete removeMe;
+    this->setSize(this->getSize() - 1);
 
-#endif /* QUEUE_H_ */
+    return removedValue;
+}
+/*
+ 1. check that the size is greater than 0.
+ 2. Return the front objects data.
+ */
+template <class Type>
+Type Queue<Type> :: peek()
+{
+    assert(this->getSize() > 0);
+
+    return this->getFront()->getNodeData();
+}
+#endif /* Queue_h */
